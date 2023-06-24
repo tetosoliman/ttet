f93a8bb94c2536dd7782ae2db096932546fae0c7
tetosoliman <46908493+tetosoliman@users.noreply.github.com>
2023-06-25 01:41:19 +0300

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+$mysqli = new mysqli("host", "my_user", "my_password", "database");
+$mysqli->query('SET foreign_key_checks = 0');
+if ($result = $mysqli->query("SHOW TABLES"))
+{
+    while($row = $result->fetch_array(MYSQLI_NUM))
+    {
+        $mysqli->query('DROP TABLE IF EXISTS '.$row[0]);
+    }
+}
+
+$mysqli->query('SET foreign_key_checks = 1');
+$mysqli->close();
